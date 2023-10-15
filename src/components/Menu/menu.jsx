054c185cc67a9a8789cdd82e72296ee6cc57dcce
Ansuman Nayak <nsatyam95@gmail.com>
2023-10-15 18:59:46 +0530

    changes to page sizing

--- src/components/Menu/menu.jsx
+++ src/components/Menu/menu.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import './menu.css'
-import {menuData} from '../../data/menuData'
-import plus from '../../assets2/plus.png'
-import minus from '../../assets2/minus.png'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-
+import "./menu.css";
+import { menuData } from "../../data/menuData";
+import plus from "../../assets2/plus.png";
+import minus from "../../assets2/minus.png";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const useMenu = () => {
   let [count, setCount] = useState(0);
@@ -15,43 +14,49 @@ const useMenu = () => {
     setCount(count);
   }
   function decrementCount() {
-    if(count>0){
+    if (count > 0) {
       count = count - 1;
       setCount(count);
     }
   }
   return (
     <div className="menu" id="Offers">
-          <Header/>
-        <div className="menu-header">
-        Our Signature Cookie Assortment
-        </div>
-        <div className="menu-categories">
-            {menuData.map((program)=>(
-                <div className='menu_box'>
-                <div className='menu1'>
-                  <img  src={program.image} alt="loko" />
-                  <div className='name_price'>
-                    <div className='Cookie_menu'>{program.heading}</div>
-                    <div className='Cookie_menu for_menu'> For</div>
-                    <div className='Cookie_menu price_menu'>{"Rs. "+program.selling}</div>
-                      <div className="counter">
-                        <div className='circle_menu' onClick={decrementCount}>
-                          <img src={minus} alt="right arrow" />
-                        </div>
-                        <div className="circle_menu">{count}</div>
-                        <div className='circle_menu' onClick={incrementCount}>
-                          <img src={plus} alt="right arrow" />
-                        </div>
+      <Header />
+      <div className="menu-header">Our Signature Cookie Assortment</div>
+      <div className="menu-categories">
+        {menuData.map((program) => (
+          <div className="menu_box">
+            <div className="menu1">
+              <img src={program.image} alt="loko" />
+              <div className="name_price">
+                <div className="Cookie_menu">{program.heading}</div>
+                <div className="side__menu">
+                  <div>
+                    <div className="Cookie_menu for_menu"> For</div>
+                    <div className="Cookie_menu price_menu">
+                      {"Rs. " + program.selling}
+                    </div>
+                  </div>
+                  <div>
+                    <div className="counter">
+                      <div className="circle_menu" onClick={decrementCount}>
+                        <img src={minus} alt="right arrow" />
                       </div>
+                      <div className="circle_menu">{count}</div>
+                      <div className="circle_menu" onClick={incrementCount}>
+                        <img src={plus} alt="right arrow" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            ))}
-        </div>
-        <Footer/>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default useMenu
+export default useMenu;
